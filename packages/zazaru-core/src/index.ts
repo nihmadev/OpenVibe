4c53fa0f9b102d586d438adcf290c146d39a8aa4
Ваше Имя <you@example.com>
2026-08-16 03:00:57 +0400

    feat(zazaru-core): add shared UI packages core

---
+++ packages/zazaru-core/src/index.ts
@@ -0,0 +1,9 @@
+export interface ZazaruPackageInfo {
+  name: string;
+  version: string;
+}
+
+export const zazaruPackage: ZazaruPackageInfo = {
+  name: "zazaru",
+  version: "0.0.1",
+} satisfies ZazaruPackageInfo;
